5556577a81a58632ebb731658181a8fc9617bcd3
flaviofm83 <flavio.medeiros@ifms.edu.br>
2022-01-25 15:01:48 -0400

    Manipula array no componente

--- src/App.js
+++ src/App.js
@@ -1,94 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
 import { Component } from 'react';
 
-// Isso aqui é um componente
-
-//lembrando que esse código parece html mas ele é JSX
-
-// O html não usa, por exemplo, classeName. A gente usa só class no html...
-
-// O componente retorna só uma elemento, mas dentro desse elemento pode ter outros elementos.
-
-// O babel é a ferramenta que transforma esse JSX em Javascript.
-
-// O Webpack é a ferramenta que faz o bundle (bundle é o cara que faz o enpacotamento dos arquivos. 
-// ele vai empacotar esses arquivos todos para publicação)
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
 
 class App extends Component {
 
 state = {
-   name: 'Ricardo Nascimento', //isso aqui é uma propriedade da classe, que aqui nós chamamos de estado.
-   cont: 0 //isso aqui é uma propriedade da classe, que aqui nós chamamos de estado.
+   posts:[
+      {
+         id: 1,
+         titulo: 'Título 1',
+         conteudo: 'Conteúdo 1'
+      },
+      {
+         id: 2,
+         titulo: 'Título 2',
+         conteudo: 'Conteúdo 2'
+      },
+      {
+         id: 3,
+         titulo: 'Título 3',
+         conteudo: 'Conteúdo 3'
+      }
+   ]
 }
 
-// A função dos métodos e atualizar valores das propriedades (que aqui chamamos de estados
-
-mudaNome = ()=>{
-   this.setState ({name: 'José', cont: 0}); //eu uso a palavra this para me referenciar aos estados da classe
-}
-
-incrementa = ()=>{ //Esse método vai incrementar o cont
-   const {cont} = this.state; //tenho que criar uma variável para mudar o valor do cont
-   // isso é uma atribuição via desestruturação java script, pode pesquisar no google. Em vez de usar esse tipo de atribuição
-   // eu poderia ter usado
-   //const cont = this.state.cont;
-
-   this.setState({cont: cont+1});
-}
-
-decrementa=()=>{
-   const {cont} = this.state;
-   this.setState({cont: cont-1});
-}
 
  render(){
-    const {name, cont}= this.state; //vc pode fazer a atribuição de dois valores em uma mesma linha
+    const {posts}= this.state; //vc pode fazer a atribuição de todos os valores em uma mesma linha
   return (
         <div className="App">
-        <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-             <p onClick={this.mudaNome}>
-             {name} {cont}
-             </p>
-             <p onClick={this.incrementa}>
-                Valor +
-             </p>
-             <p onClick={this.decrementa}>
-                Valor -
-             </p>
-           <a
-              className="App-link"
-             href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-               Learn React
-            </a>
-          </header>
+           {posts.map (post=> (
+              <div key={post.id}>
+                  <h1> {post.titulo} </h1> 
+                  <p> {post.conteudo} </p>
+              </div>
+
+              // esse key={post.id} foi utilizado para resolver o warning
+               // pois o react funciona com chave única para identificação de elementos de um objeto
+           ))}
         </div>
-       );
+       )
  }
 }
 
