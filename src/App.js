1b523b19402c1b91e586407b64db2d383a81c75b
flaviofm83 <flavio.medeiros@ifms.edu.br>
2022-01-26 15:59:34 -0400

    criacao do ciclo de vida de um componente

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Component } from 'react';
 class App extends Component {
 
 state = {
+   cont: 0,
    posts:[
       {
          id: 1,
@@ -21,14 +22,41 @@ state = {
          titulo: 'Título 3',
          conteudo: 'Conteúdo 3'
       }
+      
    ]
+}
+
+timeoutUpdate = null;
+//Esse componentDidMount é executado só uma vez (quando ele é criado)
+componentDidMount(){ //quando a gente abre a página a primeira vez ou dá um f5, esse método e chamado automaticamente
+   this.mudaTimeout();
+}
+
+//Esse componentDidMount é executado sempre que muda de estado
+componentDidUpdate(){
+   this.mudaTimeout();
+}
+
+componentWillUnmount(){
+   clearTimeout(this.timeoutUpdate);
+}
+
+mudaTimeout=()=>{
+
+   const {posts, cont} = this.state; //trouxemos os estados (ou seja, os elementos posts e cont) para dentro do método
+   posts[0].titulo = "Esse título mudou" //fizemos a alteração do título do primeiro elemento do array
+   this.timeoutUpdate=setTimeout (()=>{
+      this.setState({posts, cont: cont+1});//Atualizamos o estado com a função setState, mostrando o título alterado e incrementando o contador
+   },2000);
+
 }
 
 
  render(){
-    const {posts}= this.state; //vc pode fazer a atribuição de todos os valores em uma mesma linha
+    const {posts, cont}= this.state; //agora posts  e cont estão dentro de this.state;
   return (
         <div className="App">
+           <h1>{cont}</h1>
            {posts.map (post=> (
               <div key={post.id}>
                   <h1> {post.titulo} </h1> 
