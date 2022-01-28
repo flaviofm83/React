2c12632305a9c87d81bbb0f361f38d4f3e81b68e
flaviofm83 <flavio.medeiros@ifms.edu.br>
2022-01-27 22:35:20 -0400

    Buscando dados externos com API fetch

--- src/App.js
+++ src/App.js
@@ -5,68 +5,49 @@ import { Component } from 'react';
 class App extends Component {
 
 state = {
-   cont: 0,
-   posts:[
-      {
-         id: 1,
-         titulo: 'Título 1',
-         conteudo: 'Conteúdo 1'
-      },
-      {
-         id: 2,
-         titulo: 'Título 2',
-         conteudo: 'Conteúdo 2'
-      },
-      {
-         id: 3,
-         titulo: 'Título 3',
-         conteudo: 'Conteúdo 3'
-      }
-      
-   ]
+   posts:[]
 }
 
-timeoutUpdate = null;
-//Esse componentDidMount é executado só uma vez (quando ele é criado)
-componentDidMount(){ //quando a gente abre a página a primeira vez ou dá um f5, esse método e chamado automaticamente
-   this.mudaTimeout();
+
+componentDidMount(){ 
+  this.loadPosts();
 }
 
-//Esse componentDidMount é executado sempre que muda de estado
-componentDidUpdate(){
-   this.mudaTimeout();
-}
 
-componentWillUnmount(){
-   clearTimeout(this.timeoutUpdate);
-}
+loadPosts = async ()=>{
+   const postResponse = fetch('https://jsonplaceholder.typicode.com/posts');
+   const photoResponse = fetch('https://jsonplaceholder.typicode.com/photos');
 
-mudaTimeout=()=>{
+   const [posts, photos] = await Promise.all([postResponse, photoResponse]);
 
-   const {posts, cont} = this.state; //trouxemos os estados (ou seja, os elementos posts e cont) para dentro do método
-   posts[0].titulo = "Esse título mudou" //fizemos a alteração do título do primeiro elemento do array
-   this.timeoutUpdate=setTimeout (()=>{
-      this.setState({posts, cont: cont+1});//Atualizamos o estado com a função setState, mostrando o título alterado e incrementando o contador
-   },2000);
+   const postsJason = await posts.json();
+   const photosJason = await photos.json();
 
+   const postsAndPhotos = postsJason.map((post, index)=>{
+      return { ...post, cover: photosJason[index].url}
+   });
+
+   this.setState({posts:postsAndPhotos}); 
 }
 
 
  render(){
-    const {posts, cont}= this.state; //agora posts  e cont estão dentro de this.state;
+    const {posts}= this.state; 
   return (
-        <div className="App">
-           <h1>{cont}</h1>
+      <section className = "container">
+         <div className="posts">
            {posts.map (post=> (
-              <div key={post.id}>
-                  <h1> {post.titulo} </h1> 
-                  <p> {post.conteudo} </p>
-              </div>
-
-              // esse key={post.id} foi utilizado para resolver o warning
-               // pois o react funciona com chave única para identificação de elementos de um objeto
+              <div className='post'>
+                 <img src={post.cover} alt={post.title} />
+                  <div key={post.id} className="post-content">
+                        <h1> {post.title} </h1> 
+                        <p> {post.body} </p>
+                  </div>
+               </div>
            ))}
         </div>
+      </section>
+        
        )
  }
 }
